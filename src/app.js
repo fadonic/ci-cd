0be7df30c71832f5d8647241b9c4fb51213f43f5
unknown <fadonic@yahoo.com>
2022-01-09 13:11:37 +0100

    Initial commit

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 
 app.get('/test', (_req, res) => {
-  res.status(200).send('Hello world')
+  res
+    .status(200)
+    .send('Hello world this an introduction to CI/CD with github actions')
 })
 module.exports = app
