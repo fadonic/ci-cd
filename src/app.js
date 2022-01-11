44ab7369cb46a7377cfc7b2020913307586c0b46
unknown <fadonic@yahoo.com>
2022-01-11 10:30:06 +0100

    Initial commit

--- src/app.js
+++ src/app.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
-app.get('/test', (_req, res) => {
+var corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(cors(corsOptions))
+
+app.get('/test', (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*')
   res
     .status(200)
     .send('Hello world this is an introduction to CI/CD with github actions')
